7c2e87cde807d426f6e5202a4c9700b9a7e0c3e4
Viktorashi <33178074+viktorashi@users.noreply.github.com>
2024-10-30 00:13:44 +0200

    bothced solutio so ppl know where the server is running locally

--- open_control/__init__.py
+++ open_control/__init__.py
@@ -15,8 +15,15 @@ if __name__ == 'open_control':
         ip = subprocess.check_output(["ifconfig en0 | grep 'inet'| awk '{print$2}'"], shell=True, text=True).split("\n")[1].strip()
         #ca nu exista index 1 daca nu ai interfete pornite
     except IndexError:
-        ip = '127.0.0.1'
-        print('nu cred ca ai netu pornit da-i ca ok dam pe localhost')
+        try:
+            ip = subprocess.check_output(["ifconfig ens33 | grep 'inet'| awk '{print$2}'"], shell=True, text=True).split("\n")[1].strip()
+        except IndexError:
+            try:
+                ip = subprocess.check_output(["ifconfig ens37 | grep 'inet'| awk '{print$2}'"], shell=True, text=True).split("\n")[1].strip()
+            except IndexError:
+                ip = '127.0.0.1'
+                print('nu cred ca ai netu pornit da-i ca ok dam pe localhost')
+
     app.secret_key = os.urandom(24)
     '''
     indented because of 
